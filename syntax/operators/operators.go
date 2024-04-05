b24540e3940c8f8a31af69ea3d957739219d71c2
Mario Skrlec <mario.skrlec@tildeloop.com>
2024-04-05 12:15:28 +0200

    Added basic where clause checks

---
+++ syntax/operators/operators.go
@@ -0,0 +1,17 @@
+package operators
+
+const EqualOperator = "="
+const UnEqualOperator = "!="
+const LessThanOperator = "<"
+const GreaterThanOperator = ">"
+const GreaterThanOrEqualOperator = ">="
+const LessThanOrEqualOperator = "<="
+
+var Operators = []string{
+	EqualOperator, 
+	UnEqualOperator, 
+	LessThanOperator, 
+	LessThanOrEqualOperator, 
+	GreaterThanOperator, 
+	GreaterThanOrEqualOperator,
+}
