6c116a695358d733487db71d3022d9d3d00e4118
mario <marioskrlec222@proton.me>
2024-04-05 16:47:51 +0200

    Updated workflow and started working on conditions

--- syntax/syntaxParts/condition.go
+++ syntax/syntaxParts/condition.go
@@ -10,8 +10,6 @@ type Item interface {
 	Value() string
 	Next() Item
 	Prev() Item
-	SetNext(item Item)
-	SetPrev(item Item)
 }
 
 func (i *item) Value() string {
@@ -24,14 +22,6 @@ func (i *item) Next() Item {
 
 func (i *item) Prev() Item {
 	return i.prev
-}
-
-func (i *item) SetNext(item Item) {
-	i.next = item
-}
-
-func (i *item) SetPrev(item Item) {
-	i.prev = item
 }
 
 func NewItem(value string) Item {
