64ae7963cfc74d347f26ed0233988f692263a557
Mario Skrlec <mario.skrlec@tildeloop.com>
2024-04-12 11:27:27 +0200

    Added validation for column alias

--- cmd/main.go
+++ cmd/main.go
@@ -3,24 +3,31 @@ package main
 import (
 	"encoding/csv"
 	"errors"
-	"fmt"
+	"github.com/MarioLegenda/cig"
 	"io"
-	"log"
-	"os"
 )
 
 func main() {
-	f, err := os.Open("../testdata/example.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
+	tryCig()
+	/*	f, err := os.Open("../testdata/example.csv")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	lineReader1 := NewLineReader(f, false)
-	fmt.Println(lineReader1())
-	fmt.Println(lineReader1())
-	fmt.Println(lineReader1())
-	fmt.Println(lineReader1())
-	fmt.Println(lineReader1())
+		lineReader1 := NewLineReader(f, false)
+		fmt.Println(lineReader1())
+		fmt.Println(lineReader1())
+		fmt.Println(lineReader1())
+		fmt.Println(lineReader1())
+		fmt.Println(lineReader1())*/
+}
+
+func tryCig() {
+	sql := "SELECT      'g.Year',         'e.Industry_aggregation_NZSIOC',         'z.Industry_code_NZSIOC'      FROM path:../../../testdata/example.csv As g WHERE 'a'::int = 'b' AND 'b'::float != 'a' OR 'c'::int != 'o' AND 'C'::float <= 'O'"
+
+	c := cig.New()
+
+	c.Run(sql)
 }
 
 func NewLineReader(f io.Reader, skipColumns bool) func() ([]string, error) {
